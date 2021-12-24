33677c1722fc2996264095ff4dcc0240c73b9dac
Shashangbhagat <shashangbhagat@gmail.com>
2021-12-24 19:35:45 +0530

    Configured mongodb

--- index.ts
+++ index.ts
@@ -1,10 +1,27 @@
 import express, { Application, Request, Response } from "express";
+const mongoose = require("mongoose");
 
 const app: Application = express();
 const port = 3000;
+const mongoUrl = 
+    'mongodb+srv://vertrical:vertrical@cluster0.vggd7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(mongoUrl,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Database connected");
+});
+
 
 app.get(
     "/",
